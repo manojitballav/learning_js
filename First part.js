d9037560533423190c8e7edd037e27e7d8ca4549
Manojit Ballav <manojitocto@gmail.com>
2016-10-31 11:55:29 +0530

    First Part

---
+++ First part.js
@@ -0,0 +1,49 @@
+/*To learn the fundamentals of javascript*/
+
+/*To print a statement in the console of Google Chrome browser*/
+console.log("This is the statement that I want to print in the console of the browser");
+
+/*To generate a alert in the browser like a popup */
+alert("This will be the message in the browser");
+
+/*To create a variable in js*/
+var variablename1;
+var variablename2;
+
+/*assigning a value to the variable*/
+variablename1 = 10;
+variablename2 = "string";
+
+/*we can even perform all in the same place*/
+var variablename3 = 19098983;
+var variablename4 = "some shit string";
+
+/*to accept values from user */
+var variablename5 = prompt("Text that needs to be prompted to the user");
+
+/*typecasting*/
+var variablename6 = 1986410246012364;
+var variablename7 = number(variablename6);
+
+/*concatenate two variables*/
+alert(variablename2 + variablename5);
+
+/*concatenate a variable and a string*/
+alert("Fuck this is " + variablename4);
+
+/*Looping statements*/
+
+/*while loop*/
+var variable1 = 40;
+while(variable1!=0){
+	console.log(variable1);
+	variable1--;
+}
+alert("Done printing all the values of variable");
+
+/*for loop*/
+var var2 = 40;
+for(var x = 0; x < var2; x++){
+	console.log(x);
+}
+alert("Done printing all the values of variable");
